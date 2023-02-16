0cea1a13315da7f084212654a7efea7ecdf2488a
Mian Muhammad Nadeem <LENOVO@ahmedinternew.hnh.local>
2023-02-16 16:02:42 +0500

    ahmed

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const home = () => {
+  return (
+    <div><h1>Hello i am about page</h1>
+      <Link href="/">Go to home page</Link>
+    
+    </div>
+  )
+}
+
+export default home
